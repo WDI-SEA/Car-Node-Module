3a78cb6fc591127427d4ac246012a07dd13718e4
BeckyBoo88 <rbooco07@gmail.com>
2021-05-26 20:05:56 -0600

    working on functions

---
+++ Car.js
@@ -0,0 +1,31 @@
+class Car {
+
+    constructor("red", convertible) {
+
+        this.color = red;
+        this.convertible = true;
+        carDetails: {
+            speed: 0,
+
+        }
+    };
+        
+    function accelerate () => {
+        for (let i = 0; i < 25; i++) {
+            console.log(i)
+            i++
+        }
+    }
+
+    function decelerate () => {
+        for (let i = 0; i >= 25; i--) {
+            console.log(i)
+            i--
+        }
+    }
+
+
+
+}
+
+module.exports = Car
