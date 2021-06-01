fbe527fa6b7683367a9e7e8236d44377b7ff4b6a
shawn-stover <shawn.stover.it.cs@gmail.com>
2021-05-31 18:43:42 -0700

    Forgot to fork

---
+++ Car.js
@@ -0,0 +1,26 @@
+// Define variables and initial functions 
+class Car {
+    constructor(color, convertible) {
+    this.color = color
+    this.speed = 0
+    this.convertible = convertible
+    this.isConvertible = false
+    }
+
+    accelerate(number) {
+        this.speed += number
+    }
+
+    decelerate(number) {
+        this.speed -= number
+    }
+
+    if(convertible = true) {
+        isConvertible = true
+    }
+}
+
+module.exports = Car
+
+
+
