a360b9fe96d225974d371d1b1069a250d3a039c3
SpiralV <MVelasco90@hotmail.com>
2021-05-31 20:50:37 -0700

    finished deliverable

---
+++ Car.js
@@ -0,0 +1,16 @@
+
+module.exports = class {
+    constructor(color, convertible) {
+        this.color = color
+        this.convertible = convertible
+        this.speed = 0
+    }
+
+    accelerate(n) {
+        this.speed += n
+    }
+
+    decelerate(n) {
+        this.speed -= n
+    }
+}
