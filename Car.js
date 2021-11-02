e1f40768b1ed3cd4a1fdee51e4ba9291766d89a7
Mackmiller <mackmiller19@gmail.com>
2021-11-02 17:07:03 -0400

    add basic class functions

--- Car.js
+++ Car.js
@@ -1,11 +1,21 @@
 //car class
 class Car {
-    constructor(color, convertible) {
+    constructor(color, convertible, speed) {
         this.color = color;
         this.convertible = convertible;
         this.speed = 0;
+     }
+     accelerate(x) {
+        return this.speed + x
+     }
+     decelerate(y) {
+         return this.speed - y
      }
 }
 //car example
 const car1 = new Car("red", "yes")
 console.log("this is Car1", car1)
+
+//test accelerate and decelerate functions
+console.log("Car1 is accelerating by " + car1.accelerate(5))
+console.log("Car1 is decelerating by " + car1.decelerate(5))
