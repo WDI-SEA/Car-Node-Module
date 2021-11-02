ad146020916d4c77d035c56d1c21e09c463e797d
Mackmiller <mackmiller19@gmail.com>
2021-11-02 18:17:18 -0400

    cleaning code

--- Car.js
+++ Car.js
@@ -3,6 +3,7 @@
         constructor(color, convertible) {
             this.color = color;
             this.convertible = convertible;
+            //init speed is 0
             this.speed = 0;
         }
         accelerate(x) {
@@ -10,17 +11,17 @@
             return this.speed
          }
          decelerate(x) {
-             this.speed = this.speed - x
-             return this.speed
+            this.speed = this.speed - x
+            return this.speed
          }
     }
-// // //car example
-const car1 = new Car("red", true)
+// //car example
+// const car1 = new Car("red", true)
 
-// // //test accelerate and decelerate functions
-console.log("Car1 is accelerating by " + car1.accelerate(25))
+// //test accelerate and decelerate functions
+// console.log("Car1 is accelerating by " + car1.accelerate(25))
 
-
+//export Car class
 module.exports = Car
 
 
