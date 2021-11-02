01cfa5e6a814c4c8515098a5b532da13d393d7c6
harrdev <jasonharrphx@live.com>
2021-11-02 13:34:07 -0700

    finished, all tests pass

---
+++ Car.js
@@ -0,0 +1,13 @@
+module.exports = class Car { 
+    constructor(color, convertable, speed, accelerate, decelerate) {
+        this.color = color,
+        this.convertible = convertable,
+        this.speed = 0
+    }
+        accelerate = (num) => {
+        this.speed += num
+        }
+        decelerate = (num) => {
+        this.speed -= num
+        }
+    }
