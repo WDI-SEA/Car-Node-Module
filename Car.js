391b4bed7f321686b4e6359b988e5578c4bc8fc6
Mackmiller <mackmiller19@gmail.com>
2021-11-02 16:50:05 -0400

    basic car class test

---
+++ Car.js
@@ -0,0 +1,11 @@
+//car class
+class Car {
+    constructor(color, convertible) {
+        this.color = color;
+        this.convertible = convertible;
+        this.speed = 0;
+     }
+}
+//car example
+const car1 = new Car("red", "yes")
+console.log("this is Car1", car1)
