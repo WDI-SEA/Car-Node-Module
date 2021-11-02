8e57006da3f46fb1fc16f46dfb6050ea639d36c2
cmiltiadou <cmiltiad@gmail.com>
2021-11-02 18:58:01 -0400

    completed deliverable by making a Car.js module that included necessary functions

---
+++ Car.js
@@ -0,0 +1,14 @@
+let speed = 0
+
+module.exports=class Car {
+    constructor(c, bool, ) {
+        this.color = c,
+        this.convertible = bool,
+        this.speed = 0
+        this.accelerate = (i) =>{
+            this.speed = i + 0 };
+        this.decelerate = (i) =>{
+            this.speed = this.speed - i };
+    }
+    
+}
