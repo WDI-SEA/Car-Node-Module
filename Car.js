cb70598dc2805565b6aa314343512c9fb65a509c
devinrbopp <devinrbopp@gmail.com>
2021-11-02 18:50:43 -0600

    refactored using constructor function

--- Car.js
+++ Car.js
@@ -1,20 +1,33 @@
-class Car {
-    constructor(color, convertible) {
-        // variables for inputs
-        this.color = color
-        this.convertible = convertible
+// class Car {
+//     constructor(color, convertible) {
+//         // variables for inputs
+//         this.color = color
+//         this.convertible = convertible
 
-        // default variable
-        this.speed = 0
+//         // default variable
+//         this.speed = 0
 
-        // two functions that take a single argument and add or subtract it from the current speed
-        this.accelerate = (change) => {
-            this.speed += change
-        }
+//         // two functions that take a single argument and add or subtract it from the current speed
+//         this.accelerate = (change) => {
+//             this.speed += change
+//         }
 
-        this.decelerate = (change) => {
-            this.speed -= change
-        }
+//         this.decelerate = (change) => {
+//             this.speed -= change
+//         }
+//     }
+// }
+
+// trying again using the constructor function (newer method)
+function Car(color, convertible) {
+    this.color = color
+    this.convertible = convertible
+    this.speed = 0
+    this.accelerate = (number) => {
+        this.speed += number
+    }
+    this.decelerate = (number) => {
+        this.speed -= number
     }
 }
 
