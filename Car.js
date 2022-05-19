9cc1794d8d0422ed2eec29928a7b7b7a23156cbf
ApexBortz <nicholas@georgetownframeshoppe.com>
2022-05-18 18:37:53 -0700

    Car class made

---
+++ Car.js
@@ -0,0 +1,15 @@
+class Car {
+    constructor(color, convertible){
+        this.color = color;
+        this.convertible = convertible;
+        this.speed = 0
+    }
+
+    accelerate(acc){
+        this.speed += acc;
+    }
+    decelerate(dcc)
+        this.speed += dcc;
+}
+
+module.exports = car
