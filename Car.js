310eb09d1601d454eca282a1a2e11416759ad3f1
Hoan-K-Le <hoanleecr@gmail.com>
2022-05-18 17:10:36 -0700

    finish delivable

---
+++ Car.js
@@ -0,0 +1,19 @@
+// const { exports } = require("mocha/lib/interfaces");
+
+class Car {
+    constructor(color, convertible) {
+        this.color = color;
+        this.convertible = convertible;
+        this.speed = 0;
+        
+    }
+    accelerate(acc) {
+        this.speed = this.speed + acc
+    } 
+    decelerate(dec) {
+        this.speed = this.speed - dec
+    }
+
+}
+
+module.exports = Car;
