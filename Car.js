6e07a8317b1f3538f95a4e9e478a50ff2c7d58e9
Billy Lu <billyfwlu@gmail.com>
2022-05-18 17:15:42 -0700

    Completed assignment 1

---
+++ Car.js
@@ -0,0 +1,17 @@
+class car {
+    constructor(color,convertible){
+        this.color = color
+        this.convertible = convertible
+        this.speed = 0
+    }
+
+    accelerate(acc) {
+        this.speed = this.speed + acc
+    }
+
+    decelerate(dec){
+        this.speed = this.speed - dec
+    }
+}
+
+module.exports = car
