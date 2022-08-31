75f15808ddf8e36636573a952265b5e87c534b5d
yasahaverland <yasahaverland@icloud.com>
2022-08-31 12:42:15 -0400

    homowork Done, passed test! Yasmin H.

---
+++ Car.js
@@ -0,0 +1,17 @@
+class Car {
+    constructor(color, convertible) {
+        this.color = color;
+        this.convertible = convertible;
+        this.speed = 0;
+        }
+    accelerate(speedAcc) {
+        this.speed += speedAcc
+    }
+    decelerate(speedAcc) {
+        this.speed -= speedAcc
+        }
+}
+module.exports = Car
+
+
+
