8de71e89c76afb6acc5200d8a07e5e556fda9952
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-08-31 09:16:36 -0700

    update

--- Car.js
+++ Car.js
@@ -7,10 +7,10 @@ function Car(color, convertible, speed){
  
   
 Car.prototype.accelerate = function(accel){
-    return this.speed + accel;
+    return this.speed += (accel);
  };
  Car.prototype.decelerate = function(decel){
-    return this.speed - decel;
- };
+    return this.speed -= decel;
+ }
 
 module.exports = Car;
