4e66aae82bc16ebf8bb3437c30ef3c50720e3af4
devin-lynch <devin.l.lynch@gmail.com>
2022-08-30 19:51:45 -0500

    Create class

---
+++ Car.js
@@ -0,0 +1,15 @@
+class Car {
+    constructor(color, convertible) {
+        this.color = color
+        this.convertible = convertible
+        this.speed = 0
+    }
+    
+    accelerate(number) {
+        speed += number
+    }
+
+    decelerate(number) {
+        speed -= number
+    }
+}
