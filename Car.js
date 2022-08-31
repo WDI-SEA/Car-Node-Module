db855894f56944906627779735035924d14212d7
brandon-w1205 <bwu1205@gmail.com>
2022-08-30 17:38:13 -0700

    adds Car class w/ constructors and functions to pass tests and exports the class

---
+++ Car.js
@@ -0,0 +1,17 @@
+class Car {
+    constructor(color, convertible){
+        this.color = color
+        this.convertible = convertible
+        this.speed = 0;
+    }
+
+    accelerate(speed) {
+        this.speed += speed
+    }
+
+    decelerate(speed) {
+        this.speed -= speed
+    }
+}
+
+module.exports = Car;
