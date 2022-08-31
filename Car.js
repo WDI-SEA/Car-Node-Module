bab73105e09f65f3b384a19f94c3ebc6951f9c31
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-08-31 09:11:20 -0700

    homework

---
+++ Car.js
@@ -0,0 +1,16 @@
+function Car(color, convertible, speed){
+    this.color = color;
+    this.convertible = convertible;
+    this.speed = 0;
+ 
+}
+ 
+  
+Car.prototype.accelerate = function(accel){
+    return this.speed + accel;
+ };
+ Car.prototype.decelerate = function(decel){
+    return this.speed - decel;
+ };
+
+module.exports = Car;
