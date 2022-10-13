37777248d7ecce144e27075b023effdb2397940d
shaikhaalbuflasa <shaikhasaad86@gmail.com>
2022-10-13 07:38:37 +0300

    Car class in js

---
+++ Car.js
@@ -0,0 +1,18 @@
+module.exports = class car {
+    constructor(color, convertible){
+        this.color = color
+        this.convertible = convertible
+        this.speed = 0
+    }
+    accelerate(num){
+        this.speed=this.speed+num
+
+    }
+    decelerate(num){
+        this.speed=this.speed-num
+
+    }
+
+}
+
+//To test the Car class run 'npm test ' in the ter
