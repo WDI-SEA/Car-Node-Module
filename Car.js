b4946e153908e6c64637d31157408ec44ff54851
MramRashidR <mramrashid98@gmail.com>
2022-10-13 08:12:30 +0300

    first draft

--- Car.js
+++ Car.js
@@ -2,6 +2,18 @@ class Cars {
     constructor(color , convertible) {
         this.color = color
         this.convertible = convertible
-        
+        this.speed = 0
+    }
+
+
+    accelerate(num) {
+        this.speed += num
+    }
+
+    decelerate(num) {
+        this.speed -= num
     }
 }
+
+
+module.exports = class {}
